10a8244c9f5dbdd4054b162e6f46623e1f7d8edb
David <david.harris378@gmail.com>
2019-08-24 08:28:42 +1200

    Final changes

--- src/Poker.java
+++ src/Poker.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 
 public class Poker{
-    static char sep;
-    static String line;
-    static Card[] hand = new Card[5];
+    //static 
+    //static Card[] hand = new Card[5];
     
     public static void main(String[] args){
-        checkCard();
-    }
-
-    public static void checkCard(){
         Scanner input = new Scanner(System.in);
         while(input.hasNextLine()){
-            line = input.nextLine();
-            validateSep();
-        }
+            String line = input.nextLine();
+            String[] parts = validateSep(line);
+            if(parts == null){
+                invalid(line);
+                continue;
+            } 
+            Card[] hand = convert(parts);
+            if(hand == null){
+                invalid(line);
+                continue;
+            }
+            Card[] newHand = duplicates(hand);
+            if(newHand == null || newHand.length == 0){
+                invalid(line);
+                continue;
+            }
+            order(newHand);
+        } 
     }
 
-    public static void invalid(){
+    public static void invalid(String line){
         System.out.println("Invalid: " + line);
-        checkCard();
     }
 
     public static int convertNumber(char letter){
@@ -39,8 +48,9 @@ public class Poker{
         return 0;
     }
 
-    public static void validateSep(){
+    public static String[] validateSep(String line){
         char[] separator = {' ', '-', '/'};
+        char sep = ' ';
         int sepFound = 0;
         for(int i = 0; i < line.length(); i++){
             if(line.charAt(i) == ' ' && sepFound == 0){
@@ -52,39 +62,38 @@ public class Poker{
             }
             for(int j=0; j<separator.length; j++){
                 if(line.charAt(i) == separator[j] && line.charAt(i) != sep){
-                    invalid();
+                    return null;
                 }
             }
         }
-
         String[] parts = line.split(String.valueOf(sep));
         if(parts.length != 5){
-            invalid();
+            return null;
         }
-        convert(parts);
+        return parts;
     }
 
-    public static void convert(String[] parts){
+    public static Card[] convert(String[] parts){
         Card[] newHand = new Card[5]; 
         for(int i=0; i<5; i++){
             int finalNum = 0;
             char suit = parts[i].charAt(parts[i].length()-1);
             suit = Character.toUpperCase(suit);
             if(!String.valueOf(suit).matches("C|D|H|S")){
-                invalid();
+                return null;
             }
             if(parts[i].length() == 2){
                 char number = parts[i].charAt(0);
                 if(Character.isLetter(number)){
                     int num = convertNumber(number);
                     if(num == 0){
-                        invalid();
+                        return null;
                     }
                     finalNum = num;
                 } else {
                     int num = Character.getNumericValue(number);
                     if(num > 13 || num < 1){
-                        invalid();
+                        return null;
                     }
                     finalNum = num;
                 }
@@ -96,11 +105,11 @@ public class Poker{
                 if(num.matches("[0-9]+")){
                     int numSet = Integer.parseInt(num);
                     if(numSet >13 || numSet < 1){
-                        invalid();
+                        return null;
                     }
                     finalNum = numSet;
                 } else {
-                    invalid();
+                    return null;
                 }
             }
             newHand[i] = new Card();
@@ -111,7 +120,23 @@ public class Poker{
                 newHand[i].setNumber(finalNum);
             }
         }
-        order(newHand);
+        return newHand;
+    }
+    
+    
+    public static Card[] duplicates(Card[] newHand){
+        try {
+            for(int i=0; i<5; i++){
+                for(int j=i + 1; j<5; j++){
+                    if(newHand[i].getSuit() == newHand[j].getSuit() && newHand[i].getNumber() == newHand[j].getNumber()){
+                        return null;
+                    }
+                }
+            }
+        } catch(NullPointerException e){
+            return null;
+        }
+        return newHand;
     }
 
     public static void order(Card[] newHand){      
@@ -119,35 +144,23 @@ public class Poker{
         int n = newHand.length;        
         Card key;
        
+        //Note: This code for insertion sort came from my implementation in cosc242
         for(j = 1; j < n; ++j){
             key = newHand[j];
-            for(k = j-1; k>=0 && newHand[k].getNumber() > key.getNumber() ; k--){
+            for(k = j-1; k>=0 && newHand[k].getNumber() > key.getNumber(); k--){
                 newHand[k+1] = newHand[k];
             }
             newHand[k+1] = key;
         }
         
-        
-        //add code to order by suit if same number
-        //need to sort this better -> insertion matbe
-        for(int i = 0; i<n; i+=1){
-            for(j=1; j<n; j++){
-                if(newHand[i].getNumber() == newHand[j].getNumber()){
-                    if((int) (newHand[i].getSuit()) > (int) newHand[j].getSuit()){
-                        System.out.println(newHand[i].getSuit());
-                        System.out.println(newHand[j].getSuit());
-                        Card temp = newHand[i];
-                        newHand[i] = newHand[j];
-                        newHand[j] = temp;
-                        for (Card card : newHand) {
-                              output(card);
-                        }
-                    }
-                } else {
-                    break;
-                }
+        for(j = 1; j < n; ++j){
+            key = newHand[j];
+            for(k = j-1; k>=0 && newHand[k].getNumber()== key.getNumber()&& newHand[k].getSuit() > key.getSuit(); k--){
+                newHand[k+1] = newHand[k];
             }
+            newHand[k+1] = key;
         }
+        
         for (Card card : newHand) {
             output(card);
         }
